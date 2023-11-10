f04876cda3931ff1b019f1bdbb5d2824519f345c
sai-surya-pavuluri <spavuluri@datapac.com>
2023-11-10 09:43:30 +0000

    First half of backend Team API done

--- src/main/java/io/javabrains/ipldashboard/controller/TeamController.java
+++ src/main/java/io/javabrains/ipldashboard/controller/TeamController.java
@@ -1,11 +1,14 @@
 package io.javabrains.ipldashboard.controller;
 
 import io.javabrains.ipldashboard.model.Team;
+import io.javabrains.ipldashboard.repository.MatchRepository;
 import io.javabrains.ipldashboard.repository.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class TeamController {
@@ -13,8 +16,18 @@ public class TeamController {
     @Autowired
     private TeamRepository teamRepository;
 
+    @Autowired
+    private MatchRepository matchRepository;
+
     @GetMapping("/team/{teamName}")
     public Team getTeam(@PathVariable String teamName){
-        return teamRepository.findByTeamName(teamName);
+      Team team = teamRepository.findByTeamName(teamName);
+      team.setMatches(matchRepository.findLatestMatchesByTeam(teamName, 4));
+      return team;
+    }
+
+    @GetMapping("/team")
+    public Iterable<Team> getAllTeam(){
+        return teamRepository.findAll();
     }
 }
